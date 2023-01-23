d088ef22ad2b99e444472b176af7eece660e3409
zefonsecasj <csjuniod@gmail.com>
2023-01-23 15:01:49 -0300

    ZeModoro Version 1.0

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # ZeModoro
 <h1>Objective</h1>
-<p> The main purpose of this project is just to learn and practice a little bit more about web tecnologies, so feel confortable to suggest some features, improvements in this code or use it as a base for your codes👨🏻‍💻 </p>
+<p> The main purpose of this project is just to learn and practice a little bit more about web tecnologies, so feel confortable to suggest some features, improvements in this code/page layout or use it as a base for your codes👨🏻‍💻 </p>
 <h1>About</h1>
 <p>The Pomodoro is a technique for studying (or just concentrate/focus) on a task. The idea is to divide some large windows to focus and intercalate them with small windows to relax and, after 3 cycles of windows, take a large relax window.</p>
 <p>Since this a simple project (just to train some fundamentals) i'm just using base technologies as HTML, CSS and JS (without any major framework).</p>
+<h1>How it works</h1>
+<p>The pomodoro has three states (Work Time, Small Break and Long Break) and all the logic and layout work around this idea. So we have different layouts for each state and we can set the state we want to be.</p>
+<h1>Work Time</h1>
+<img src="./images/work_time_layout.png">
+<h1>Small Break</h1>
+<img src="./images/small_break_layout.png">
+<h1>Long Break</h1>
+<img src="./images/long_break_layout.png">
+
+
--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var enable_counter = true
 var updateCounterId = null
 var state = 1
 var pomoRounds = 0
+var workTime = 25
+var smallBreak = 5
+var longBreak = 30
 
 function change_state(id){
     if(id=='focus_btn'){
@@ -26,13 +29,13 @@ function start_counter(){
         let counterMin
         switch (state){
             case(1):
-                counterMin = 25
+                counterMin = workTime
                 break
             case(2):
-                counterMin = 5
+                counterMin = smallBreak
                 break
             case(3):
-                counterMin = 30
+                counterMin = longBreak
                 break
         }
         updateCounterId = setInterval(counter,10, counterMin, counterSec)
@@ -51,13 +54,13 @@ function clear_counter(){
     clearInterval(updateCounterId)
     switch (state){
         case(1):
-            window.document.getElementById('Counter').innerText = "25:00"
+            window.document.getElementById('Counter').innerText = time_stamper(workTime)
             break
         case(2):
-            window.document.getElementById('Counter').innerText = "05:00"
+            window.document.getElementById('Counter').innerText = time_stamper(smallBreak)
             break
         case(3):
-            window.document.getElementById('Counter').innerText = "30:00"
+            window.document.getElementById('Counter').innerText = time_stamper(longBreak)
             break
     }
     enable_counter = true
@@ -65,29 +68,29 @@ function clear_counter(){
 
 function counter(firstMin, fullSec){ 
     if(min > firstMin){
-        min = 0
+        min = 1
         clearInterval(updateCounterId)
         enable_counter = true
         switch (state){
             case(1):
                 pomoRounds++
                 if(pomoRounds%4 == 0){
-                    window.document.getElementById('Counter').innerText = "30:00"
+                    window.document.getElementById('Counter').innerText = time_stamper(longBreak)
                     state = 3
                 }
                 else{
-                    window.document.getElementById('Counter').innerText = "05:00"
+                    window.document.getElementById('Counter').innerText = time_stamper(smallBreak)
                     state = 2
                 }
                 window.document.getElementById('PomoRounds').innerText = `Pomodoro Rounds: #${pomoRounds}`
                 break
             case(2):
                 state = 1
-                window.document.getElementById('Counter').innerText = "25:00"
+                window.document.getElementById('Counter').innerText = time_stamper(workTime)
                 break
             case(3):
                 state = 1
-                window.document.getElementById('Counter').innerText = "25:00"
+                window.document.getElementById('Counter').innerText = time_stamper(workTime)
                 break
         } 
         background_styling(state)
@@ -105,8 +108,7 @@ function counter(firstMin, fullSec){
     }
 }
 
-
-function time_stamper(min,second){
+function time_stamper(min,second=0){
     let outputText
     if(min>=10){
         outputText = `${min}:`
@@ -124,31 +126,43 @@ function time_stamper(min,second){
     return outputText
 }
 
-function change_settings(){
-    window.alert('TEste')
+function set_pomo(){
+    var pomoAdjust = window.document.getElementById('focus_input').value
+    workTime = pomoAdjust
+    clear_counter()
+    background_styling(state)
 }
 
+function set_break(){
+    var brkAdjust = window.document.getElementById('brk_input').value
+    smallBreak = brkAdjust
+    clear_counter()
+    background_styling(state)
+}
 
 function background_styling(state){
     var centerDiv = window.document.getElementById('center_div').style
     switch(state){
         case(1):
-            window.document.getElementById('Counter').innerText = "25:00"
-            centerDiv.background = "url('./work_test.jpg')"
+            window.document.getElementById('Counter').innerText = time_stamper(workTime,0)
+            centerDiv.background = "url('./images/work_test.jpg')"
             centerDiv.backgroundSize = "cover"
             document.body.style.backgroundColor = '#5E6D72'
+            document.getElementById('center-text').innerText = "You Gotta Work!"
             break
         case(2):
-            window.document.getElementById('Counter').innerText = "05:00"
-            centerDiv.background = "url('./small_break.jpg')"
+            window.document.getElementById('Counter').innerText = time_stamper(smallBreak,0)
+            centerDiv.background = "url('./images/small_break.jpg')"
             centerDiv.backgroundSize = "cover"
-            document.body.style.backgroundColor = '#9BBBDB'
+            document.body.style.backgroundColor = '#90847A'
+            document.getElementById('center-text').innerText = "Coffe Break!"
             break
         case(3):
-            window.document.getElementById('Counter').innerText = "30:00"    
-            centerDiv.background = "url('./nap_test.jpg')"
+            window.document.getElementById('Counter').innerText = time_stamper(longBreak,0)   
+            centerDiv.background = "url('./images/nap_test.jpg')"
             centerDiv.backgroundSize = "cover"
             document.body.style.backgroundColor = '#555938'
+            document.getElementById('center-text').innerText = "Nap Break!"
             break
     }
 
